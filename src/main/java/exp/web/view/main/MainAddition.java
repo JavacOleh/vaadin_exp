6a04cb9c6bc82916ea8ceef42ec9c4a868dfe7c7
Oleh <olehvasylevych1337@gmail.com>
2026-04-04 23:05:29 +0300

    final version. Done: Need to add class names or other unique keys to buttons, fields, etc.. after that i'll be able to start finishing autotests.
    maybe somewhere it's possible to do refactoring :)
    
    p2

--- src/main/java/exp/web/view/main/MainAddition.java
+++ src/main/java/exp/web/view/main/MainAddition.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.data.provider.ListDataProvider;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import exp.util.AuthUtil;
 import exp.web.entity.Person;
@@ -34,47 +35,66 @@ public class MainAddition {
         view.grid.setWidthFull();
         view.grid.setHeight("400px");
 
-        view.hashTagIndColumn = view.grid.addColumn(person -> people.indexOf(person) + 1)
-                .setHeader("#")
+        // 🔹 Колонки с одинаковыми id
+        view.hashTagIndColumn = view.grid.addColumn(
+                        new ComponentRenderer<>(person -> {
+                            Span span = new Span(String.valueOf(people.indexOf(person) + 1));
+                            span.getElement().setAttribute("id", "cell_index"); // общий id для всех ячеек
+                            return span;
+                        })
+                ).setHeader("#")
                 .setTextAlign(ColumnTextAlign.CENTER);
 
-        view.firstNameColumn = view.grid.addColumn(Person::getFirstName)
-                .setHeader("First Name")
+        view.firstNameColumn = view.grid.addColumn(
+                        new ComponentRenderer<>(person -> {
+                            Span span = new Span(person.getFirstName());
+                            span.getElement().setAttribute("id", "cell_first_name"); // общий id
+                            return span;
+                        })
+                ).setHeader("First Name")
                 .setTextAlign(ColumnTextAlign.CENTER);
 
-        view.lastNameColumn = view.grid.addColumn(Person::getLastName)
-                .setHeader("Last Name")
+        view.lastNameColumn = view.grid.addColumn(
+                        new ComponentRenderer<>(person -> {
+                            Span span = new Span(person.getLastName());
+                            span.getElement().setAttribute("id", "cell_last_name"); // общий id
+                            return span;
+                        })
+                ).setHeader("Last Name")
                 .setTextAlign(ColumnTextAlign.CENTER);
 
-        view.ageColumn = view.grid.addColumn(Person::getAge)
-                .setHeader("Age")
+        view.ageColumn = view.grid.addColumn(
+                        new ComponentRenderer<>(person -> {
+                            Span span = new Span(String.valueOf(person.getAge()));
+                            span.getElement().setAttribute("id", "cell_age"); // общий id
+                            return span;
+                        })
+                ).setHeader("Age")
                 .setTextAlign(ColumnTextAlign.CENTER);
 
+        // 🔹 Actions
         view.actionsColumn = view.grid.addComponentColumn(MainActionsAddition.getActionsColumn(view))
                 .setHeader(new HorizontalLayout(new Span("Actions")) {{
                     setWidthFull();
                     setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
                 }});
 
+        // 🔹 Фильтр и кнопка "New Person"
         var filterWrapper = new HorizontalLayout();
         filterWrapper.setWidthFull();
         filterWrapper.setSpacing(true);
         filterWrapper.setPadding(false);
 
-        // Фильтр занимает всю доступную ширину
         view.filterText.setWidthFull();
-
         view.doNewPerson.setId("open_dialog_create_person_button_id");
-
         view.doNewPerson.addClickListener(e -> {
             Person person = new Person();
             MainActionsAddition.openEditDialog(person, view, true);
         });
 
         filterWrapper.add(view.filterText, view.doNewPerson);
-        filterWrapper.expand(view.filterText); // фильтр растягивается, кнопка занимает только свою ширину
+        filterWrapper.expand(view.filterText);
 
-        // Убираем старое добавление фильтра в layout, теперь добавляем filterWrapper
         view.form_wrapper.removeAll();
         view.form_wrapper.add(filterWrapper, view.grid);
     }
